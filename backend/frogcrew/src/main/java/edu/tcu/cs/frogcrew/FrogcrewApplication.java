db572829ea1d25e2a5103d60c752b60e3d5941fb
sameep-git <sameep.shah@tcu.edu>
2025-04-02 16:55:02 -0500

    added a backend folder and made a spring boot project in it

---
+++ backend/frogcrew/src/main/java/edu/tcu/cs/frogcrew/FrogcrewApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.frogcrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogcrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrogcrewApplication.class, args);
+    }
+
+}
---
+++ backend/frogcrew/src/test/java/edu/tcu/cs/frogcrew/FrogcrewApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.frogcrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogcrewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
